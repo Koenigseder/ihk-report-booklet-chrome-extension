e3fe6e71000fd1e3379476bddca1c8b26c7e87c7
Koenigseder <kevin.koenigseder@outlook.com>
2023-09-20 18:03:07 +0200

    Restructure files and folders

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,7 @@
 const btnGroups = document.getElementById("btn-show-groups");
 btnGroups.addEventListener("click", () => {
   window.open(
-    `chrome-extension://${chrome.runtime.id}/groups/groups.html`,
+    `chrome-extension://${chrome.runtime.id}/src/groups/groups.html`,
     "_blank"
   );
 });
